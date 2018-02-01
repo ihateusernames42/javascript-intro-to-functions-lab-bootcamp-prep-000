c4a4f5352fb79c8c1fc162e5d1fbdb40a7df3e26
Dylan L Jones <darkraikiri@gmail.com>
2018-02-01 00:52:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ return string.toLowerCase
 function sayHiToGrandma(string){
   if (string.toLowerCase===string){
     return "i cant hear you!"}
-  if (string.)
+  if (string.toUpperCase===string){
+    return "YES INDEED!"
+  }
   }
 }
